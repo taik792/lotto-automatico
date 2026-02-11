6b0c664148299313c96ffa1f701a55beb526ebfa
taik792 <rosario79@libero.it>
2026-02-11 18:34:42 +0100

    Update aggiorna_lotto.py

--- .github/workflows/aggiorna_lotto.py
+++ .github/workflows/aggiorna_lotto.py
@@ -1,44 +1,34 @@
 import requests
 import json
-import csv
-from io import StringIO
 
 FILE = "storico.json"
-MAX_ESTRAZIONI = 5
-
-CSV_URL = "https://raw.githubusercontent.com/matteocontrini/lotto-data/master/lotto.csv"
 
 RUOTE = [
     "Bari","Cagliari","Firenze","Genova","Milano",
     "Napoli","Palermo","Roma","Torino","Venezia","Nazionale"
 ]
 
-def salva(data):
+def salva_storico(data):
     with open(FILE, "w") as f:
         json.dump(data, f, indent=2)
 
 def scarica():
-    r = requests.get(CSV_URL, timeout=20)
-    r.raise_for_status()
+    url = "https://www.superenalotto.it/estrazioni/lotto"
+    r = requests.get(url)
+    testo = r.text
 
-    csv_data = csv.DictReader(StringIO(r.text))
-    righe = list(csv_data)[-MAX_ESTRAZIONI:]
+    dati = {r: [] for r in RUOTE}
 
-    risultato = {r: [] for r in RUOTE}
+    for ruota in RUOTE:
+        if ruota in testo:
+            dati[ruota] = [1,2,3,4,5]  # temporaneo per test
 
-    for riga in righe:
-        for ruota in RUOTE:
-            key = ruota.lower()
-            if key in riga and riga[key]:
-                numeri = [int(n) for n in riga[key].split()]
-                risultato[ruota] = numeri
-
-    return risultato
+    return dati
 
 dati = scarica()
-salva(dati)
+salva_storico(dati)
+print("Aggiornato!")
 
-print("Aggiornamento completato")
 
 
 
