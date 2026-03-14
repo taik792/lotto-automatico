7339d984eaa30114691a9dc7eefbd463b3d64f94
taik792 <rosario79@libero.it>
2026-03-14 22:53:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import json
+from itertools import combinations
+from collections import Counter
 
 ordine_ruote = [
 "Bari",
@@ -49,21 +51,25 @@ for ruota in ordine_ruote:
 
     saturazione = round(sum(freq.values())/len(freq),2)
 
-    # -----------------------
+    # ------------------------
     # CICLOMETRIA RUOTA
-    # -----------------------
+    # ------------------------
 
     a = ultima[0]
     b = ultima[1]
 
     d = abs(a-b)
+
     d2 = 90-d
 
     c1 = (b+d)%90
     c2 = (b+d2)%90
 
-    if c1==0: c1=90
-    if c2==0: c2=90
+    if c1 == 0:
+        c1 = 90
+
+    if c2 == 0:
+        c2 = 90
 
     ciclometria_ruota = [
         f"{a}-{b}",
@@ -71,18 +77,36 @@ for ruota in ordine_ruote:
         f"{b}-{c2}"
     ]
 
+    # ------------------------
+    # AMBI STATISTICI
+    # ------------------------
+
+    ultime200 = dati[-200:]
+
+    conta_ambi = Counter()
+
+    for estr in ultime200:
+        for ambo in combinations(estr,2):
+            conta_ambi[tuple(sorted(ambo))] += 1
+
+    top_ambi = []
+
+    for (x,y),freq_ambo in conta_ambi.most_common(5):
+        top_ambi.append(f"{x}-{y}")
+
     risultati.append({
         "ruota":ruota,
         "ultima":ultima,
         "numeri_caldi":numeri_caldi,
         "ambo_forte":ambo_forte,
+        "ambi_statistici":top_ambi,
         "ciclometria":ciclometria_ruota,
         "saturazione":saturazione
     })
 
-# -----------------------
+# ------------------------
 # CICLOMETRIA TRA RUOTE
-# -----------------------
+# ------------------------
 
 ciclometria_tra_ruote = []
 
@@ -107,8 +131,8 @@ for r1,r2 in coppie_ruote:
         })
 
 output = {
-"ruote": risultati,
-"ciclometria_ruote": ciclometria_tra_ruote
+    "ruote":risultati,
+    "ciclometria_ruote":ciclometria_tra_ruote
 }
 
 with open("risultati.json","w") as f:
