176e3e7808b6fa5102acb5b08d776a4570b9357b
taik792 <rosario79@libero.it>
2026-03-14 10:34:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import json
 
-# ordine ruote lotto
 ordine_ruote = [
-    "Bari",
-    "Cagliari",
-    "Firenze",
-    "Genova",
-    "Milano",
-    "Napoli",
-    "Palermo",
-    "Roma",
-    "Torino",
-    "Venezia"
+"Bari",
+"Cagliari",
+"Firenze",
+"Genova",
+"Milano",
+"Napoli",
+"Palermo",
+"Roma",
+"Torino",
+"Venezia"
 ]
 
-# carica estrazioni
 with open("estrazioni.json") as f:
     estrazioni = json.load(f)
 
@@ -24,59 +22,57 @@ for ruota in ordine_ruote:
 
     dati = estrazioni[ruota]
 
-    # ultima estrazione
     ultima = dati[-1]
 
-    # ultime 30 estrazioni
-    ultime_estrazioni = dati[-30:]
+    ultime = dati[-30:]
 
-    frequenze = {}
+    freq = {}
 
-    for estr in ultime_estrazioni:
-        for numero in estr:
-            frequenze[numero] = frequenze.get(numero, 0) + 1
+    for estr in ultime:
+        for n in estr:
+            freq[n] = freq.get(n,0)+1
 
-    # ordina frequenze
-    ordinati = sorted(frequenze.items(), key=lambda x: x[1], reverse=True)
+    ordinati = sorted(freq.items(), key=lambda x:x[1], reverse=True)
 
     numeri_caldi = [ordinati[0][0], ordinati[1][0]]
 
-    ambo_forte = f"{numeri_caldi[0]} - {numeri_caldi[1]}"
+    ambo_forte = f"{numeri_caldi[0]}-{numeri_caldi[1]}"
 
-    saturazione = round(sum(frequenze.values()) / len(frequenze), 2)
+    saturazione = round(sum(freq.values())/len(freq),2)
 
-    # --------------------
+    # -------------------
     # CICLOMETRIA
-    # --------------------
+    # -------------------
 
     a = ultima[0]
     b = ultima[1]
 
-    distanza = abs(b - a)
+    d = abs(a-b)
+    d2 = 90-d
 
-    c1 = (b + distanza) % 90
-    c2 = (c1 + distanza) % 90
+    c1 = (b + d) % 90
+    c2 = (b + d2) % 90
 
-    if c1 == 0:
-        c1 = 90
-    if c2 == 0:
-        c2 = 90
+    c3 = (a + d) % 90
+    c4 = (a + d2) % 90
 
-    ambi_ciclometrici = [
-        f"{a}-{b}",
-        f"{b}-{c1}",
-        f"{c1}-{c2}"
-    ]
+    ciclometrici = []
+
+    for x,y in [(a,b),(b,c1),(b,c2),(a,c3),(a,c4)]:
+
+        if x==0: x=90
+        if y==0: y=90
+
+        ciclometrici.append(f"{x}-{y}")
 
     risultati.append({
-        "ruota": ruota,
-        "ultima": ultima,
-        "numeri_caldi": numeri_caldi,
-        "ambo_forte": ambo_forte,
-        "saturazione": saturazione,
-        "ciclometria": ambi_ciclometrici
+        "ruota":ruota,
+        "ultima":ultima,
+        "numeri_caldi":numeri_caldi,
+        "ambo_forte":ambo_forte,
+        "ciclometria":ciclometrici,
+        "saturazione":saturazione
     })
 
-# salva risultati
-with open("risultati.json", "w") as f:
-    json.dump(risultati, f, indent=4)
+with open("risultati.json","w") as f:
+    json.dump(risultati,f,indent=4)
