79288882f5f77b782d92fa4f82bef5045aa8926a
taik792 <rosario79@libero.it>
2026-03-25 11:42:46 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,6 @@
-ULTIME_ESTRAZIONI = 300  # puoi mettere 15 o 20
+ULTIME_ESTRAZIONI = 300  # puoi mettere anche 500
 
 def prendi_ultime_estrazioni(lista_estrazioni):
-    """
-    Ritorna solo le ultime N estrazioni
-    """
     if not lista_estrazioni:
         return []
     return lista_estrazioni[-ULTIME_ESTRAZIONI:]
