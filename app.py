c5d00f2151df59604cd90a2ec3b1dc12f81fd23e
taik792 <rosario79@libero.it>
2026-03-25 13:49:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import json
 from analisi_ambi import analizza_ruote
 
+
 def main():
     with open("estrazioni.json") as f:
         dati = json.load(f)
 
-    ruote, top = analizza_ruote(dati)
-
-    output = {
-        "ruote": ruote,
-        "giocate_top": top
-    }
+    risultati = analizza_ruote(dati)
 
     with open("risultati.json", "w") as f:
-        json.dump(output, f, indent=2)
+        json.dump(risultati, f, indent=2)
+
 
 if __name__ == "__main__":
     main()
