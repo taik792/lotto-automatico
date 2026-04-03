3a1a4e0b7f0cdeb8d261000478ef4af83dcdde08
taik792 <rosario79@libero.it>
2026-04-03 12:10:01 +0200

    Update genera_risultati.py

--- genera_risultati.py
+++ genera_risultati.py
@@ -24,7 +24,7 @@ for ruota in RUOTE:
 
     # ultime estrazioni
     ultime = estrazioni_ruota[-1]
-    storico = estrazioni_ruota[-100:]  # ultime 20
+    storico = estrazioni_ruota[-20:]  # ultime 20
 
     # ===== FREQUENZE =====
     freq = {}
