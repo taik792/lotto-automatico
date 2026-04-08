440acaeea0b80959a72d475a8a0064ee3314dd56
taik792 <rosario79@libero.it>
2026-04-08 20:10:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,58 +2,38 @@ fetch("risultati.json")
   .then(res => res.json())
   .then(data => {
 
-    // 🧾 ESTRAZIONI
+    // ESTRAZIONI
     const estrDiv = document.getElementById("estrazioni");
 
     for (const r in data.ultime_estrazioni) {
-      const div = document.createElement("div");
-      div.className = "card";
-
-      div.innerHTML = `
-        <div class="title-small">${r}</div>
-        <div class="numbers">${data.ultime_estrazioni[r].join(" - ")}</div>
+      estrDiv.innerHTML += `
+        <div class="card">
+          <b>${r}</b><br>
+          ${data.ultime_estrazioni[r].join(" - ")}
+        </div>
       `;
-
-      estrDiv.appendChild(div);
     }
 
-    // 🔗 GEMELLE
-    const gemDiv = document.getElementById("gemelle");
-
-    for (const r in data.ruote_gemelle) {
-      const div = document.createElement("div");
-      div.innerHTML = `${r} → ${data.ruote_gemelle[r]}`;
-      gemDiv.appendChild(div);
-    }
-
-    // ⭐ AMBI
+    // AMBI CON RUOTA
     const ambiDiv = document.getElementById("ambi");
 
-    data.top_ambi.forEach(a => {
-      const div = document.createElement("div");
-      div.className = "card top";
-
-      div.innerHTML = `
-        <div class="numbers">${a.numeri.join(" - ")}</div>
-        <div class="percent">${a.prob}%</div>
+    data.top.forEach(a => {
+      ambiDiv.innerHTML += `
+        <div class="card top">
+          <b>${a.ruota}</b><br>
+          ${a.numeri.join(" - ")}<br>
+          ${a.prob}%
+        </div>
       `;
-
-      ambiDiv.appendChild(div);
     });
 
-    // 🔥 TERNI
+    // JOLLY
     const terniDiv = document.getElementById("terni");
 
-    data.top_terni.forEach(t => {
-      const div = document.createElement("div");
-      div.className = "card";
-
-      div.innerHTML = `
-        <div class="numbers">${t.numeri.join(" - ")}</div>
-        <div class="percent">${t.prob}%</div>
-      `;
-
-      terniDiv.appendChild(div);
-    });
+    terniDiv.innerHTML = `
+      <div class="card">
+        🎯 Ruota Jolly: <b>${data.ruota_jolly}</b>
+      </div>
+    `;
 
   });
