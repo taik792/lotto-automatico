ca3a348d302efec8cff8f1eb45720b93845ae7f7
taik792 <rosario79@libero.it>
2026-04-29 09:31:48 +0200

    Update genera_risultati.py

--- genera_risultati.py
+++ genera_risultati.py
@@ -43,8 +43,8 @@ for ruota in RUOTE:
 
     ultime = estrazioni_ruota[-1]
 
-    breve = estrazioni_ruota[-1:]
-    medio = estrazioni_ruota[-2000:]
+    breve = estrazioni_ruota[-10:]
+    medio = estrazioni_ruota[-1500:]
     lungo = estrazioni_ruota[-3000:]
 
     freq_breve = calcola_freq(breve)
